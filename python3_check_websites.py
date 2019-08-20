161ef48b81de991ce6128f61be08dabbd874b0bf
arrheni <arrheni@users.noreply.github.com>
2019-08-20 09:24:48 +0800

    Create python3_check_websites.py

---
+++ python3_check_websites.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import urllib2
+import time
+ 
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+file = open('addr.txt')
+lines = file.readlines()
+aa=[]
+print('--------------------------开始第一次检查--------------------------')
+for line in lines:
+	if line.find('http')!=(-1):
+		index=line.index('http')
+		url=line[index:].replace('\n','')
+		try:
+			code=urllib2.urlopen(url).getcode()
+			print(url+'\tstatus code is: '+bytes(code))
+		except urllib2.URLError:
+			print(url+'\t无法访问 ')
+		except urllib2.HTTPError:
+			print(url+'\t无法访问 ')
+	else:
+		continue
+	
+	aa.append(url)
+ 
+time.sleep(2)
+ 
+print('--------------------------开始第二次检查--------------------------')
+for a in aa:
+	tempUrl = a
+	try :
+		opener.open(tempUrl)
+		print(tempUrl+'\t没问题')
+	except urllib2.HTTPError:
+		print(tempUrl+'\t访问页面出错')
+		time.sleep(2)
+	except urllib2.URLError:
+		print(tempUrl+'\t访问页面出错')
+		time.sleep(2)
+	time.sleep(0.1)
