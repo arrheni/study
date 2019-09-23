3314fb7fa320c3f704ae511fe04f944ef33f983e
arrheni <arrheni@users.noreply.github.com>
2019-09-24 03:10:11 +0800

    Update deploy_increment_zip_or_unzip_war_or_rsync.sh

--- deploy_increment_zip_or_unzip_war_or_rsync.sh
+++ deploy_increment_zip_or_unzip_war_or_rsync.sh
@@ -3,153 +3,147 @@
 d=$(date +%F)
 #d="2019-08-14"
 
-#mother_path="/home/fb/jenkins/workspace/Deploy_Cluster_AP2_QRY_97.47_AppSrv03/qry-web/target/qry-web/"
-#base_path="/home/fb/jenkins/workspace/Deploy_Cluster_AP2_QRY_97.47_AppSrv03/"
-#text_path="${base_path}qry/$d/"
-
 base_path=`pwd`
-mother_path="$base_path/local-webapp/target/local-webapp-0.5.5.3/"
-war_file="local-webapp-0.5.5.3.war"
-text_path="${base_path}/unzip_jar/$d/"
+mother_path="$base_path/qry-web/target/qry-web/"
 
-rsync_path="${text_path}/qry-web/"
-#war_path="${base_path}/qry-web/target/"
-remote_path='was@10.1.95.47:/home/ap/was/AppServer/profiles/AppSrv03/installedApps/qrytap0Node03Cell/qry-web.ear/qry-web.war/'
+war_file="qry-web.war"
+list_file="list_qry_$d.txt"
+jar_list="jar_qry.txt"
+increment_zip="qry-web_increment_$d.zip"
+
+backup_path="${base_path}/Backup/$d/"
+rsync_path="${backup_path}Rsync/"
+update_list="update_list_$d.txt"
+java_list="java_list_$d.txt"
+#META_resources=(pa cs clm cap common mobcss images)
+META_resources=(common)
+
+
 
 #########################################################################################################
 from_path="WEB-INF/lib/"
 to_class_path="WEB-INF/classes/"
-
 META_resources_path="WEB-INF/classes/META-INF/resources/"
+
+webapp="src/main/webapp/"
+resources="src/main/resources/"
+java="src/main/java/"
+
 
 function clean_path()
 {
-if [ -d $text_path ] 
-then
-	rm -rf ${text_path}*
-else
-	mkdir -p $text_path
-fi
+	if [ -d $backup_path ] 
+	then
+		rm -rf ${backup_path}*
+	else
+		mkdir -p $backup_path
+	fi
 }
 
 ############################## check svn delete files. need list_qry_$d.txt
 function check_del_file()
 {
-grep "D       " list_qry_$d.txt | tee delete_list_$d.txt
+	grep "D       " $list_file | tee ibm-partialapp-delete
+	
+	if ( [ -s ibm-partialapp-delete ] )
+	then
+		echo "-------------------------there is deleted files ABOVE------------------------------------"
 
-if ( [ -s delete_list_$d.txt ] )
-then
-	echo "there is deleted files ABOVE-----------------------------------------------------"
-	echo "---------------------------------------------------------------------------------"
-	sed -i.bak "/D       /d" list_qry_$d.txt
-fi
+		sed -i.bak "/D       /d" $list_file
+
+		grep "$webapp" ibm-partialapp-delete | awk -F "$webapp" '{print $2}' > ibm-partialapp-delete.props
+		grep "$resources" ibm-partialapp-delete | awk -F "$resources" '{print "WEB-INF/classes/" $2}' >> ibm-partialapp-delete.props
+		grep "$java" ibm-partialapp-delete | awk -F "$java" '{print "WEB-INF/classes/" $2}' > $java_list
+		rm ibm-partialapp-delete	
+
+		cd $mother_path
+		awk -F "[.]" '{system("ls " $1 "*.class")}'  ${base_path}/$java_list  >>${base_path}/ibm-partialapp-delete.props
+		
+		cd $base_path
+		cp ibm-partialapp-delete.props ${mother_path}META-INF 
+	fi
 }
 
 ############################## unzip war & jar.   need svn_jar.txt
 function unzip_jar()
 {
-#unzip -qo ${war_path}qry-web.war -d ${war_path}qry-web
-cat svn_jar.txt|while read line
-do
-	unzip_jar=`ls ${mother_path}${from_path}${line}*`
-	unzip -qo  ${unzip_jar}  -d ${mother_path}${to_class_path}
-#	if [ -d ${mother_path}${META_resources_path} ] then;
-#		mv ${mother_path}${META_resources_path}* ${mother_path}
-#	fi
-	mv ${unzip_jar}  ${text_path}
-done
-
-
-#cd ${mother_path}${META_resources_path}; 
-#find . -type d -exec mkdir -p ${mother_path}/\{} \; 
-#find . -type f -exec mv \{} ${mother_path}/\{} \; 
-#find . -type d -empty -delete
-#cd $base_path
-
-rd=(pa cs clm cap common mobcss images)
-for resources_dir in ${rd[@]}
-do
-        if      [ -d ${mother_path}${resources_dir} ]
-        then
-                #mv  ${mother_path}${META_resources_path}${resources_dir}/*  ${mother_path}${resources_dir}
-                cp -rl  ${mother_path}${META_resources_path}${resources_dir}  ${mother_path}${resources_dir}
-                rm -r  ${mother_path}${META_resources_path}${resources_dir}
-
-        else
-                mkdir ${mother_path}${resources_dir}
-                #mv  ${mother_path}${META_resources_path}${resources_dir}/*  ${mother_path}${resources_dir}
-                cp -rl  ${mother_path}${META_resources_path}${resources_dir}  ${mother_path}${resources_dir}
-                rm -r  ${mother_path}${META_resources_path}${resources_dir}
-
-
-        fi
-done
-
-
+	#unzip -qo ${war_path}qry-web.war -d ${war_path}qry-web
+	cat ${jar_list}|while read line
+	do
+		unzip_jar=`ls ${mother_path}${from_path}${line}*`
+		unzip -qo  ${unzip_jar}  -d ${mother_path}${to_class_path}
+		mv ${unzip_jar}  ${backup_path}
+	done
+	
+	for mr in ${META_resources[@]}
+	do
+	        cp -r ${mother_path}${META_resources_path}${mr}  ${mother_path}
+	        rm -r ${mother_path}${META_resources_path}${mr}
+	
+	done
 
 }
 
 ############################## get file copy list
 
-webapp="src/main/webapp/"
-resources="src/main/resources/"
-java="src/main/java/"
-resources_M_r="src/main/resources/META-INF/resources"
-
-function get_and_check_cp_list()
+function get_and_check_update_list()
 {
-grep "$webapp" list_qry_$d.txt | awk -F "$webapp" '{print $2}'  >  cp_$d.txt 
-grep "$resources_M_r" list_qry_$d.txt | awk -F "$resources_M_r" '{print $2}' >> cp_$d.txt
-grep "$resources" list_qry_$d.txt | grep -v "$resources_M_r" | awk -F "$resources" '{print "WEB-INF/classes/" $2}' >> cp_$d.txt 
-grep "$java" list_qry_$d.txt | awk -F "$java" '{print "WEB-INF/classes/" $2}' > cp_java_$d.txt
-sed "s/\.java/\.class/g" cp_java_$d.txt >> cp_$d.txt
-
-cd $mother_path
-awk -F "[.]" '{system("ls " $1 "*.class")}'  ${base_path}cp_java_$d.txt  | grep '\$' | tee -a  ${base_path}cp_$d.txt
-
-cd $base_path
-wc list_qry_$d.txt delete_list_$d.txt cp_java_$d.txt  cp_$d.txt 
-
+	grep "$webapp" $list_file | awk -F "$webapp" '{print $2}' > $update_list
+	grep "$resources" $list_file | awk -F "$resources" '{print "WEB-INF/classes/" $2}' >> $update_list
+	grep "$java" $list_file | awk -F "$java" '{print "WEB-INF/classes/" $2}' > $java_list
+	
+	cd $mother_path
+	awk -F "[.]" '{system("ls " $1 "*.class")}'  ${base_path}/$java_list  >>${base_path}/$update_list
+	
+	cd $base_path
+	if [ -s ibm-partialapp-delete.props ] ; then 
+		echo "META-INF/ibm-partialapp-delete.props" >> $update_list
+	fi
+	wc ${list_file}* ibm-partialapp-delete.props $java_list $update_list
+	
+	echo "------------------check the files numbers----------------------"
+	grep '\$' $update_list 
 }
 
 ############################## rsync xml js jsp class to rsync_path
 function rsync_file()
 {
-mkdir -p $rsync_path
-rsync --files-from=cp_$d.txt $mother_path $rsync_path 
-
-mv list_qry_$d.txt delete_list_$d.txt cp_java_$d.txt  cp_$d.txt $text_path
+	mkdir -p $rsync_path
+	rsync --files-from=$update_list $mother_path $rsync_path 
+	
+	mv ${list_file}* ibm-partialapp-delete.props $java_list $update_list $backup_path
 }
 
 ############################## zip increment file to .zip and jar .war 
 function zip_and_jar_file()
 {
-cd $rsync_path
-[ -f qry-web_increment_$d.zip ] && rm qry-web_increment_$d.zip
-zip -qr qry-web_increment_$d.zip  ./ 
-mv qry-web_increment_$d.zip $text_path
-cd $mother_path
-jar -cfM0 $war_file ./
-mv $war_file $text_path
+	cd $rsync_path
+	[ -f $increment_zip ] && rm -v $increment_zip
+	zip -qr $increment_zip  ./ 
+	mv $increment_zip $backup_path
+	
+	cd $mother_path
+	jar -cfM0 $war_file ./
+	mv $war_file $backup_path
+
+	cd $base_path
 }
 
 ############################## deploy zip or war
 function deploy()
 {
-cd $text_path
-
-rsync --files-from=cp_$d.txt $rsync_path $remote_path
+	cd $backup_path
 
 }
 
-if  [  -f list_qry_$d.txt ]
-then
-    clean_path
-    check_del_file
-    unzip_jar
-    get_and_check_cp_list
-    rsync_file
-    zip_and_jar_file
+if  [  -r $list_file -a -r $jar_list ] ; then
+	clean_path
+	unzip_jar
+
+	check_del_file
+	get_and_check_update_list
+	rsync_file
+	zip_and_jar_file
 else
-  echo "NEED file  list_qry_$d.txt  ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--"
+  	echo "NEED file  $list_file  and  $jar_list  ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--ERR--"
 fi
