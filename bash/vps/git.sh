beaea4aa860727bb7e5e8548cffa32a8a2aa346a
cool <20747748+arrheni@users.noreply.github.com>
2024-01-05 00:10:41 -0500

    add rn

---
+++ bash/vps/git.sh
@@ -0,0 +1,6 @@
+  git clone git@github.com:arrheni/study.git
+  git add .
+  git config --global user.name "cool"
+  git config --global user.email "20747748+arrheni@users.noreply.github.com"
+  git commit -m "add rn"
+  git push origin master
