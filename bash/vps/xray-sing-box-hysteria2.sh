7366258aad6825eda9633b94d72e0b40be450b88
cool <20747748+arrheni@users.noreply.github.com>
2024-03-26 17:27:56 +0800

    install xray sing-box hysteria2

---
+++ bash/vps/xray-sing-box-hysteria2.sh
@@ -0,0 +1,3 @@
+bash <(curl -fsSL https://get.hy2.sh/)
+bash <(curl -fsSL https://sing-box.app/deb-install.sh)
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
